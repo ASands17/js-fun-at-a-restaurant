9507dbbb841b7b19accb17afb72145614bc20ba6
Amanda Sands <amandajean555@gmail.com>
2022-03-25 08:27:11 -0600

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,26 @@
-function nameMenuItem() {
+function nameMenuItem(tastyMenuItem) {
+  return `Delicious ${tastyMenuItem}`
+};
 
+function createMenuItem(newItemName, menuItemPrice, menuItemType) {
+  var menuItem = {
+  name: newItemName,
+  price: menuItemPrice,
+  type: menuItemType,
+ };
+  return menuItem;
 }
 
+function addIngredients(ingredient, listIngredients) {
+  listIngredients.push(ingredient);
+  return listIngredients;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
