8d5a2717e2212fd415fae6e049f922ee19c727bc
Amanda Sands <amandajean555@gmail.com>
2022-03-26 10:39:24 -0600

    pass refund order unit test suites

--- src/order.js
+++ src/order.js
@@ -9,11 +9,22 @@ function takeOrder(orderObject, arrayOfOrders) {
 
 
 
+function refundOrder(orderNumToRemove, arrayOfOrders) {
+  for (var i = 0; i < arrayOfOrders.length; i++) {
+    var allPrices = arrayOfOrders[i].orderNumber;
+    if (allPrices === orderNumToRemove) {
+    arrayOfOrders.splice(i, 1);
+    return arrayOfOrders;
+    }
+  }
+}
+
+
 
 
 module.exports = {
  takeOrder,
- //refundOrder,
+ refundOrder,
   // listItems,
   // searchOrder
 }
