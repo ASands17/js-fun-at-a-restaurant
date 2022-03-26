a9e7cd363152a21b9640603de204fbaa71a1c072
Amanda Sands <amandajean555@gmail.com>
2022-03-26 09:43:25 -0600

    add passing unit tests for meal section

--- src/meal.js
+++ src/meal.js
@@ -11,16 +11,39 @@ function createMenuItem(newItemName, menuItemPrice, menuItemType) {
   return menuItem;
 }
 
+
 function addIngredients(ingredient, listIngredients) {
-  listIngredients.push(ingredient);
+  if (listIngredients.includes(ingredient) === false) {listIngredients.push(ingredient);
   return listIngredients;
+} else {
+  return listIngredients;
+}
+}
+
+function formatPrice(initialPrice) {
+var formattedPrice = `$${initialPrice}`;
+return formattedPrice;
+}
+
+function decreasePrice(initialPrice) {
+  var decreasedPrice = (initialPrice - (.1 * initialPrice));
+  return decreasedPrice;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var newRecipeObject = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+  return newRecipeObject;
 }
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
--- src/order.js
+++ src/order.js
@@ -1,7 +1,24 @@
 
+
+/*function takeOrder(orderObject, arrayOfOrders) {
+  arrayOfOrders.push(orderObject);
+  return arrayOfOrders;
+
+}
+
+function takeOrder(orderObject, arrayOfOrders) {
+  arrayOfOrders.push(orderObject);
+  return arrayOfOrders; */
+
+}
+
+
+
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+ takeOrder,
+ refundOrder,
   // listItems,
   // searchOrder
 }
