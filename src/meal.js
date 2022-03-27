25bb3495ceeccb534bceded63952475c58d0e144
Amanda Sands <amandajean555@gmail.com>
2022-03-27 12:47:47 -0600

    refactor meal.js section'
    git status

--- src/meal.js
+++ src/meal.js
@@ -1,28 +1,29 @@
 function nameMenuItem(tastyMenuItem) {
   return `Delicious ${tastyMenuItem}`
-};
+}
 
 function createMenuItem(newItemName, menuItemPrice, menuItemType) {
   var menuItem = {
-  name: newItemName,
-  price: menuItemPrice,
-  type: menuItemType,
- };
+    name: newItemName,
+    price: menuItemPrice,
+    type: menuItemType,
+  };
   return menuItem;
-}
+ }
 
 
 function addIngredients(ingredient, listIngredients) {
-  if (listIngredients.includes(ingredient) === false) {listIngredients.push(ingredient);
-  return listIngredients;
-} else {
-  return listIngredients;
-}
-}
+  if (listIngredients.includes(ingredient) === false) {
+    listIngredients.push(ingredient);
+    return listIngredients;
+  } else {
+    return listIngredients;
+  }
+ }
 
 function formatPrice(initialPrice) {
-var formattedPrice = `$${initialPrice}`;
-return formattedPrice;
+  var formattedPrice = `$${initialPrice}`;
+  return formattedPrice;
 }
 
 function decreasePrice(initialPrice) {
